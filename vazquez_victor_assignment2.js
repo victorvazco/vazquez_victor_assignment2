acbc97f38fa60aa45111855e7a8bad4de542a274
victorvazco <v_vazquezcobian@yahoo.com>
2014-08-14 20:32:11 -0400

    Procedure done

---
+++ vazquez_victor_assignment2.js
@@ -0,0 +1,43 @@
+
+//Victor Vazquez
+//SDI 1408
+//Project 2
+//Deliverable 2
+// This project is my code taken from the flowchart provided
+
+
+
+//Global Variables
+var string;
+var number = 5;
+var truth = true;
+
+
+
+//Procedure
+
+var greetU = function(ask){
+    
+    console.log("Welcome back to The Awesome Game " + ask + "." );
+    
+};
+
+
+//Boolean Function
+
+
+
+//Number Function
+
+
+
+//String Function
+
+
+
+
+//Main Code
+
+var ask = prompt("What is your username?","");
+
+var callGreet = greetU(ask);
