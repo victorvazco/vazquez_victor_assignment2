bb89253ca1107299492f68b964a622aec30df38a
victorvazco <v_vazquezcobian@yahoo.com>
2014-08-14 21:07:50 -0400

    Boolean Function done

--- vazquez_victor_assignment2.js
+++ vazquez_victor_assignment2.js
@@ -8,7 +8,7 @@
 
 
 //Global Variables
-var string;
+var name = "Lee";
 var number = 5;
 var truth = true;
 
@@ -18,13 +18,41 @@ var truth = true;
 
 var greetU = function(ask){
     
-    console.log("Welcome back to The Awesome Game " + ask + "." );
+    if (ask === name) {
+        
+        console.log("Welcome back to The Awesome Game " + ask + "." );
+    } else {
+        
+        console.log("Welcome to The Awesome Game App " + ask + ".");
+    }
+    
     
 };
 
 
 //Boolean Function
 
+var decideU = function(confirmAnswer){
+    
+    var cost = 4;
+    
+    if (confirmAnswer === truth) {
+        while (cost>0) {
+            console.log("You purchased New level " + cost + ".");
+            cost --;
+            returned = 'Levels uploaded';
+        }
+        return returned;
+    
+    } else {
+        
+        var offer = " levels are waiting for you next time if you pay.";
+        cost = 4 + offer;
+        
+       returned1 = cost; 
+    }
+    return returned1;
+};
 
 
 //Number Function
@@ -38,6 +66,15 @@ var greetU = function(ask){
 
 //Main Code
 
-var ask = prompt("What is your username?","");
+var ask = prompt("What is your username?",""); //Prompt
 
-var callGreet = greetU(ask);
+var callGreet = greetU(ask);//Procedure
+
+var decideAsk = confirm("Do you want to pay extra?");
+
+var callDecide = decideU(decideAsk);
+
+console.log(callDecide + ask);
+
+
+
